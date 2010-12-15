685c805714f6980a425a454aa9c592b93afd44b9
Stella Laurenzo <stella@laurenzo.org>
2010-12-15 10:28:31 -0700

    Add skeleton main class

---
+++ src/net/rcode/assetserver/standalone/ServerMain.java
@@ -0,0 +1,60 @@
+package net.rcode.assetserver.standalone;
+
+import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import org.eclipse.jetty.server.Server;
+
+/**
+ * Main executable for starting a development server
+ * @author stella
+ *
+ */
+public class ServerMain {
+	private PrintWriter console;
+	private InetAddress listenAddress;
+	private int httpPort=4000;
+	private Server server;
+	
+	public ServerMain() {
+		console=new PrintWriter(System.out, true);
+	}
+	
+	public void setConsole(PrintWriter console) {
+		this.console = console;
+	}
+	public PrintWriter getConsole() {
+		return console;
+	}
+	
+	public void start() throws Exception {
+		// Figure listen address
+		InetSocketAddress sa;
+		if (listenAddress==null) sa=new InetSocketAddress(httpPort);
+		else sa=new InetSocketAddress(listenAddress, httpPort);
+		console.format("Starting assetserver on %s\n", sa);
+		
+		// Instantiate
+		server=new Server(sa);
+		
+		// Start
+		server.start();
+	}
+	
+	public void join() throws InterruptedException {
+		server.join();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ServerMain server=new ServerMain();
+		try {
+			server.start();
+		} catch (BindException e) {
+			server.getConsole().format("FATAL: Unable to bind socket: %s\n", e.getMessage());
+			System.exit(100);
+		}
+		server.join();
+	}
+}
