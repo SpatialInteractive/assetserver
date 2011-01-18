069b0ceb0aca28505ac0c037da52dae2d9d58cb9
Stella Laurenzo <stella@laurenzo.org>
2011-01-18 09:08:50 -0800

    Add skeleton htmlpack addon

---
+++ addons/htmlpack/src/net/rcode/assetserver/addon/htmlpack/HtmlPackAddon.java
@@ -0,0 +1,24 @@
+package net.rcode.assetserver.addon.htmlpack;
+
+import net.rcode.assetserver.addon.Addon;
+import net.rcode.assetserver.core.AssetServer;
+
+/**
+ * Main addon class for HtmlPack addon
+ * @author stella
+ *
+ */
+public class HtmlPackAddon implements Addon {
+
+	@Override
+	public String getAddonName() {
+		return "htmlpack";
+	}
+
+	@Override
+	public void configure(AssetServer server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
